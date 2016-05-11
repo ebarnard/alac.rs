72864d5df748b8e77d7e41d87933b50f3ce031be
Edward Barnard <eabarnard@gmail.com>
2016-05-11 22:22:54 +0100

    Make DecoderConfig fields private and add accessors

--- src/lib.rs
+++ src/lib.rs
@@ -7,17 +7,17 @@ pub use dec::{Decoder, Sample};
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct DecoderConfig {
-    pub frame_length: u32,
-    pub compatible_version: u8,
-    pub bit_depth: u8,
-    pub pb: u8, // rice_history_mult
-    pub mb: u8, // rice_initial_history
-    pub kb: u8, // rice_limit
-    pub num_channels: u8,
-    pub max_run: u16,
-    pub max_frame_bytes: u32,
-    pub avg_bit_rate: u32,
-    pub sample_rate: u32,
+    frame_length: u32,
+    compatible_version: u8,
+    bit_depth: u8,
+    pb: u8, // rice_history_mult
+    mb: u8, // rice_initial_history
+    kb: u8, // rice_limit
+    num_channels: u8,
+    max_run: u16,
+    max_frame_bytes: u32,
+    avg_bit_rate: u32,
+    sample_rate: u32,
 }
 
 impl DecoderConfig {
@@ -67,5 +67,48 @@ impl DecoderConfig {
             avg_bit_rate: reader.read_u32::<BigEndian>().unwrap(),
             sample_rate: reader.read_u32::<BigEndian>().unwrap(),
         })
+    }
+
+    pub fn sample_rate(&self) -> u32 {
+        self.sample_rate
+    }
+
+    pub fn bit_depth(&self) -> u8 {
+        self.bit_depth
+    }
+
+    pub fn channels(&self) -> u8 {
+        self.num_channels
+    }
+
+    pub fn max_frames_per_packet(&self) -> u32 {
+        self.frame_length
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::DecoderConfig;
+
+    #[test]
+    fn test_from_cookie() {
+        let cookie_bytes = include_bytes!("../tests/data/magic_cookie.bin");
+        let cookie = DecoderConfig::from_cookie(cookie_bytes).unwrap();
+
+        let comparison = DecoderConfig {
+            frame_length: 4096,
+            compatible_version: 0,
+            bit_depth: 16,
+            pb: 40,
+            mb: 10,
+            kb: 14,
+            num_channels: 2,
+            max_run: 255,
+            max_frame_bytes: 0,
+            avg_bit_rate: 0,
+            sample_rate: 44100
+        };
+
+        assert_eq!(cookie, comparison);
     }
 }
